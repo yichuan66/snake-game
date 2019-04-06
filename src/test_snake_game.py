a03ddb72c34b3351308b7c96e2651387b78ebaee
yichuan99 <wycgithub@gmail.com>
2019-04-05 23:03:58 -0700

    Update test_snake_game.py
    setup unified test framework

--- src/test_snake_game.py
+++ src/test_snake_game.py
@@ -182,90 +182,155 @@ class TestSnake(TestCase):
             self.assertTrue(loc in snake.body_table)
             self.assertTrue(snake.body_queue[init_body_length - i - 1] == loc)
 
-    def test_set_move_direction(self):
+    def test_set_move_direction_body_length_is_one(self):
         """
         length equal to one -- can set all 4 direction
         """
-        # arrange
-        snake = Snake(init_body_length=1, board_dimension=3)
-        key_to_tuple = {'Up': (-1, 0), 'Down': (1, 0), 'Left': (0, -1), 'Right': (0, 1), 'None': (0, 0)}
-        clockwise_keys = ['Up', 'Right', 'Down', 'Left']
-        counter_clockwise_keys = ['Up', 'Left', 'Down', 'Right']
-        opposite_keys = ['Up', 'Down', 'Right', 'Left']
-        same_keys = ['Up', 'Up', 'Down', 'Down', 'Right', 'Right', 'Left', 'Left']
-        all_key_arrangements = [clockwise_keys, counter_clockwise_keys, opposite_keys, same_keys]
+        def test_case(test_case_name, set_key_sequence, key_to_direction_mapping):
+            return {
+                'test_case_name': test_case_name,
+                'set_key_sequence': set_key_sequence,
+                'key_to_direction_mapping': key_to_direction_mapping
+            }
 
-        # act and assert
-        for key_arrangement in all_key_arrangements:
-            for key_input in key_arrangement:
-                direction = key_to_tuple[key_input]
+        def test_logic(self, test_case_):
+            snake = Snake(init_body_length=1, board_dimension=3)
+            for key_input in test_case_['set_key_sequence']:
+                direction = test_case_['key_to_direction_mapping'][key_input]
                 snake.set_move_direction(direction)
                 self.assertEqual(snake.move_direction, direction)
 
+        # test cases
+        key_to_direction_mapping = {'Up': (-1, 0), 'Down': (1, 0), 'Left': (0, -1), 'Right': (0, 1), 'None': (0, 0)}
+        test_cases = [
+            test_case('set keys clockwise',
+                      ['Up', 'Right', 'Down', 'Left'],
+                      key_to_direction_mapping),
+            test_case('set keys counter clockwise',
+                      ['Up', 'Left', 'Down', 'Right'],
+                      key_to_direction_mapping),
+            test_case('set opposite keys in sequence',
+                      ['Up', 'Down', 'Right', 'Left'],
+                      key_to_direction_mapping),
+            test_case('set same keys twice',
+                      ['Up', 'Up', 'Down', 'Down', 'Right', 'Right', 'Left', 'Left'],
+                      key_to_direction_mapping)
+        ]
+
+        # arrange
+        for tc in test_cases:
+            test_logic(self, tc)
+
+    def test_set_move_direction_body_length_more_than_one(self):
         """
         length longer than one -- can only set 3 direction, not neck direction
         """
-        # arrange
-        snake = Snake(init_body_length=1, board_dimension=3)
-        direction_list = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        neck_position_list = [(0, 1), (2, 1), (1, 0), (1, 2)]
-        head = (1, 1)
+        def test_case(test_case_name, head, neck_position, all_directions):
+            return {
+                'test_case_name': test_case_name,
+                'head': head,
+                'neck_position': neck_position,
+                'all_directions': all_directions
+            }
 
-        # act and assert
-        for neck in neck_position_list:
-            snake.body_queue = deque([head, neck])
-            snake.body_table = {head: 1, neck: 1}
+        def test_logic(test, test_case_):
+            snake = Snake(init_body_length=1, board_dimension=3)
+            head_ = test_case_['head']
+            neck_ = test_case_['neck_position']
+            all_directions_ = test_case_['all_directions']
+
+            snake.body_queue = deque([head_, neck_])
+            snake.body_table = {head_: 1, neck_: 1}
             snake.move_direction = (0, 0)
 
-            for direction in direction_list:
+            for direction in all_directions_:
                 snake.set_move_direction(direction)
                 if snake.is_opposite_direction(direction, snake.get_current_direction()):
-                    self.assertNotEqual(direction, snake.move_direction)
+                    test.assertNotEqual(direction, snake.move_direction)
                 else:
-                    self.assertEqual(direction, snake.move_direction)
+                    test.assertEqual(direction, snake.move_direction)
+
+        # test cases
+        shared_all_directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        shared_head = (1, 1)
+        test_cases = [
+            test_case('', shared_head, (0, 1), shared_all_directions),
+            test_case('', shared_head, (2, 1), shared_all_directions),
+            test_case('', shared_head, (1, 0), shared_all_directions),
+            test_case('', shared_head, (1, 2), shared_all_directions)
+        ]
+
+        for tc in test_cases:
+            test_logic(self, tc)
 
     def test_get_current_direction(self):
-        # arrange
-        snake = Snake(init_body_length=1, board_dimension=3)
 
-        # correct direction with corresponding neck position when head position is (1, 1)
-        direction_list = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        neck_position_list = [(0, 1), (2, 1), (1, 0), (1, 2)]
-        head = (1, 1)
+        def test_case(test_case_name, head, neck_position, expected_direction):
+            return {
+                'test_case_name': test_case_name,
+                'head': head,
+                'neck_position': neck_position,
+                'expected_direction': expected_direction
+            }
 
-        # act and assert
-        for i in range(len(neck_position_list)):
-            neck = neck_position_list[i]
-            direction = direction_list[i]
+        def test_logic(self, test_case_):
+
+            # arrange
+            snake = Snake(init_body_length=1, board_dimension=3)
+
+            # act and assert
+            head = test_case_['head']
+            neck = test_case_['neck_position']
+            direction = test_case_['expected_direction']
             snake.body_queue = deque([head, neck])
             snake.body_table = {head: 1, neck: 1}
             self.assertEqual(snake.get_current_direction(), direction)
+
+        test_cases = [
+            test_case('test get Up direction', (1, 1), (0, 1), (1, 0)),
+            test_case('test get Down direction', (1, 1), (2, 1), (-1, 0)),
+            test_case('test get Right direction', (1, 1), (1, 0), (0, 1)),
+            test_case('test get Left direction', (1, 1), (1, 2), (0, -1))
+        ]
+
+        for tc in test_cases:
+            test_logic(self, tc)
 
     def test_get_head_neck_diff(self):
         # arrange
         board_dimension = 3
         snake = Snake(board_dimension=board_dimension)
 
-        # act and assert
+        def test_case(test_case_name, head, neck, direction):
+            return {'test_case_name': test_case_name,
+                    'head': head,
+                    'neck': neck,
+                    'direction': direction}
+
+        def test_logic(test, test_case_):
+            test.assertEqual(snake.get_head_neck_diff(test_case_['head'],
+                                                      test_case_['neck']),
+                                                      test_case_['direction'])
+
         """        
         the head and neck location here are generated, not real head 
         and neck of the snake because we are just testing head/neck
         difference, there is no need to move the whole snake        
         """
 
-        head_neck_list_cross_edge = [[(1, 0), (1, 2), (0, 1)],
-                                     [(1, 2), (1, 0), (0, -1)],
-                                     [(0, 1), (2, 1), (1, 0)],
-                                     [(2, 1), (0, 1), (-1, 0)]]
+        # test case data
+        test_cases = [test_case('head pointing right while crossing boarder', (1, 0), (1, 2), (0, 1)),
+                      test_case('head pointing left while crossing boarder', (1, 2), (1, 0), (0, -1)),
+                      test_case('head pointing down while crossing boarder', (0, 1), (2, 1), (1, 0)),
+                      test_case('head pointing up while crossing boarder', (2, 1), (0, 1), (-1, 0)),
+                      test_case('head pointing down while NOT crossing boarder', (1, 1), (0, 1), (1, 0)),
+                      test_case('head pointing left while NOT crossing boarder', (1, 1), (1, 2), (0, -1)),
+                      test_case('head pointing up while NOT crossing boarder', (1, 1), (2, 1), (-1, 0)),
+                      test_case('head pointing right while NOT crossing boarder', (1, 1), (1, 0), (0, 1))]
 
-        head_neck_list_not_cross_edge = [[(1, 1), (0, 1), (1, 0)],
-                                     [(1, 1), (1, 2), (0, -1)],
-                                     [(1, 1), (2, 1), (-1, 0)],
-                                     [(1, 1), (1, 0), (0, 1)]]
-
-        for test_case_arrangement in [head_neck_list_cross_edge, head_neck_list_not_cross_edge]:
-            for [head, neck, direction] in test_case_arrangement:
-                self.assertEqual(snake.get_head_neck_diff(head, neck), direction)
+        # run tests
+        for tc in test_cases:
+            test_logic(self, tc)
 
     def test_head_location(self):
         # arrange
@@ -302,53 +367,74 @@ class TestSnake(TestCase):
         self.assertEqual(snake.next_step(), ((head_location[0] + 1) % board_dimension, head_location[1]))
 
     def test_move(self):
-        # test normal move
-        # arrange
-        snake = Snake(init_body_length=1, board_dimension=3)
-        snake.body_queue = deque([(1, 1), (0, 1)])
-        snake.body_table = {(1, 1): 1, (0, 1): 1}
-        snake.move_direction = (1, 0)
 
-         # act
-        snake.move(hit_fruit=False)
+        def test_case(test_case_name,
+                      init_body_queue,
+                      init_body_table,
+                      init_move_direction,
+                      hit_fruit,
+                      expected_body_queue,
+                      expected_body_table,
+                      expected_body_length,
+                      expected_is_bite_self):
+            return {'test_case_name': test_case_name,
+                    'init_body_queue': init_body_queue,
+                    'init_body_table': init_body_table,
+                    'init_move_direction': init_move_direction,
+                    'hit_fruit': hit_fruit,
+                    'expected_body_queue': expected_body_queue,
+                    'expected_body_table': expected_body_table,
+                    'expected_body_length': expected_body_length,
+                    'expected_is_bite_self': expected_is_bite_self}
 
-        # assert
-        self.assertEqual(snake.body_queue, deque([(2, 1), (1, 1)]))
-        self.assertEqual(snake.body_table, {(2, 1): 1, (1, 1): 1})
-        self.assertEqual(snake.get_body_length(), 2)
-        self.assertEqual(snake.is_bite_self(), False)
+        def test_logic(test, test_case_):
+            # arrange
+            snake = Snake(init_body_length=1, board_dimension=3)
+            snake.body_queue = test_case_['init_body_queue']
+            snake.body_table = test_case_['init_body_table']
+            snake.move_direction = test_case_['init_move_direction']
 
-        # test hit fruit move
-        # arrange
-        snake = Snake(init_body_length=1, board_dimension=3)
-        snake.body_queue = deque([(1, 1), (0, 1)])
-        snake.body_table = {(1, 1): 1, (0, 1): 1}
-        snake.move_direction = (1, 0)
+             # act
+            snake.move(hit_fruit=test_case_['hit_fruit'])
 
-         # act
-        snake.move(hit_fruit=True)
+            # assert
+            test.assertEqual(snake.body_queue, test_case_['expected_body_queue'])
+            test.assertEqual(snake.body_table, test_case_['expected_body_table'])
+            test.assertEqual(snake.get_body_length(), test_case_['expected_body_length'])
+            test.assertEqual(snake.is_bite_self(), test_case_['expected_is_bite_self'])
 
-        # assert
-        self.assertEqual(snake.body_queue, deque([(2, 1), (1, 1), (0, 1)]))
-        self.assertEqual(snake.body_table, {(2, 1): 1, (1, 1): 1, (0, 1): 1})
-        self.assertEqual(snake.get_body_length(), 3)
-        self.assertEqual(snake.is_bite_self(), False)
+        test_cases = [
+            test_case('test normal move',
+                      deque([(1, 1), (0, 1)]),
+                      {(1, 1): 1, (0, 1): 1},
+                      (1, 0),
+                      False,
+                      deque([(2, 1), (1, 1)]),
+                      {(2, 1): 1, (1, 1): 1},
+                      2,
+                      False),
+            test_case('test hit fruit',
+                      deque([(1, 1), (0, 1)]),
+                      {(1, 1): 1, (0, 1): 1},
+                      (1, 0),
+                      True,
+                      deque([(2, 1), (1, 1), (0, 1)]),
+                      {(2, 1): 1, (1, 1): 1, (0, 1): 1},
+                      3,
+                      False),
+            test_case('test bite self',
+                      deque([(1, 1), (0, 1), (0, 0), (1, 0), (2, 0)]),
+                      {(1, 1): 1, (0, 1): 1, (0, 0): 1, (1, 0): 1, (2, 0): 1},
+                      (0, -1),
+                      False,
+                      deque([(1, 0), (1, 1), (0, 1), (0, 0), (1, 0)]),
+                      {(1, 1): 1, (0, 1): 1, (0, 0): 1, (1, 0): 2},
+                      5,
+                      True)
+        ]
 
-        # test bite self move
-        # arrange
-        snake = Snake(init_body_length=1, board_dimension=3)
-        snake.body_queue = deque([(1, 1), (0, 1), (0, 0), (1, 0), (2, 0)])
-        snake.body_table = {(1, 1): 1, (0, 1): 1, (0, 0): 1, (1, 0): 1, (2, 0): 1}
-        snake.move_direction = (0, -1)
-
-         # act
-        snake.move(hit_fruit=False)
-
-        # assert
-        self.assertEqual(snake.body_queue, deque([(1, 0), (1, 1), (0, 1), (0, 0), (1, 0)]))
-        self.assertEqual(snake.body_table, {(1, 1): 1, (0, 1): 1, (0, 0): 1, (1, 0): 2})
-        self.assertEqual(snake.get_body_length(), 5)
-        self.assertEqual(snake.is_bite_self(), True)
+        for tc in test_cases:
+            test_logic(self, tc)
 
     def test_is_bite_self(self):
         # arrange
